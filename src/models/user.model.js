4242e9b22ad6d6a41c762777bb5256632a6c199f
Saket kumar <kumarsaket601@gmail.com>
2023-12-11 16:39:03 +0530

    import bcyrpt, jwt  and middleware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -46,4 +47,11 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 export const User = mongoose.model("User", userSchema);
