9d27cf30faa1d9fa2cc3c8dd37981aa678223741
Saket kumar <kumarsaket601@gmail.com>
2023-12-16 13:52:23 +0530

    added multer and cloudinary feature

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+// a function to upload file on server system which means local system
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,26 @@
+import { Multer } from "multer";
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+// this is for cloudinary configuration setup
+cloudinary.config({
+  cloud_name: process.env.CLOUNDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUNDINARY_API_KEY,
+  api_secret: process.env.CLOUNDINARY_API_SECRET,
+});
+
+// a function  to upload file on cloudinary cloud
+
+const uploadOnCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+    console.log(response);
+  } catch (error) {
+    fs.unlinkSync(filePath);
+  }
+};
+
+export { uploadOnCloudinary };
